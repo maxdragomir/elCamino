48b2029344609a6863ceaaf39d6d0322a7aceb79
Vladyslav Turok <v2rockua@gmail.com>
2020-07-22 14:11:43 +0300

    cards "bookmaker" in progress

--- index.php
+++ index.php
@@ -1741,8 +1741,7 @@
            @before-open="bCardsOpen"
            @before-close="bCardsClose"
            @opened="cardsOpened"
-           @closed="cardsClosed"
-    >
+           @closed="cardsClosed">
 
       <section class="ec-comics ec-comics_cards ec-comics_cards4">
         <div class="ec-slider">
@@ -1774,6 +1773,75 @@
                 <div class="ec-card" v-else="cards.situation === 3">
                   <div class="ec-card__part" style="background-image: url(img/el-camino/cards/city/putana/screen3/group3/bg.png)"></div>
                   <div class="ec-card__part ec-comics__slide-part ec-comics__slide-part_anim_f ec-comics__slide-part_delay_6 ec-comics__slide-part_dur_20" style="background-image: url(img/el-camino/cards/city/putana/screen3/group3/elem1.png)"></div>
+                </div>
+              </div>
+            </div>
+            <div class="ec-slider__txt">
+              <p>
+                Вас подрезал встречный автомобиль. Вы не справились с управлением и врезались в столб!
+                <span class="ec-slider__sum" v-bind:class="{'ec-slider__sum_add' : bet.change > 0, 'ec-slider__sum_rem' : bet.change < 0}">{{bet.change}} рублей</span>
+              </p>
+            </div>
+            <div class="ec-slider__bot" style="background-image: url(img/el-camino/slider/bot-img1.png)"></div>
+          </div>
+
+          <div class="ec-comics__int">
+            <button class="ec-comics__btn ec-comics__btn_close" @click="closeCards('city_2')">
+              <svg viewBox="0 0 60 60" class="ec-comics__btn_round">
+                <path class="ec-comics__round-path" stroke-linecap="round" d="M30.03,3C44.93,3.02,57,15.1,57,30c0,14.91-12.09,27-27,27S3,44.91,3,30C3,15.1,15.07,3.02,29.96,3"/>
+              </svg>
+              <svg viewBox="0 0 18 18" class="ec-comics__btn-svg">
+                <use xlink:href="#ec-comics__close"></use>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </section>
+
+    </modal>
+
+
+
+    <modal name="city_4"
+           :adaptive="true"
+           height="auto"
+           width="100%"
+           :click-to-close="false"
+           @before-open="bCardsOpen"
+           @before-close="bCardsClose"
+           @opened="cardsOpened"
+           @closed="cardsClosed">
+
+      <section class="ec-comics ec-comics_cards ec-comics_cards4">
+        <div class="ec-slider">
+          <div class="ec-slider__in">
+            <div class="ec-slider__top" style="background-image: url(img/el-camino/cards/city/bookmaker/top-img.png)"></div>
+            <div class="ec-slider__content">
+              <div class="ec-slider__item">
+                <div class="ec-card">
+                  <div class="ec-card__part" style="background-image: url(img/el-camino/cards/city/bookmaker/screen1/bg.png)"></div>
+                  <div class="ec-card__part ec-comics__slide-part ec-comics__slide-part_anim_lt ec-comics__slide-part_dur_32" style="background-image: url(img/el-camino/cards/city/bookmaker/screen1/elem1.png)"></div>
+                </div>
+              </div>
+              <div class="ec-slider__item">
+                <div class="ec-card">
+                  <div class="ec-card__part" style="background-image: url(img/el-camino/cards/city/bookmaker/screen2/bg.png)"></div>
+                  <div class="ec-card__part ec-comics__slide-part ec-comics__slide-part_anim_lf ec-comics__slide-part_dur_20" style="background-image: url(img/el-camino/cards/city/bookmaker/screen2/elem1.png)"></div>
+                  <div class="ec-card__part ec-comics__slide-part ec-comics__slide-part_anim_rf ec-comics__slide-part_dur_20" style="background-image: url(img/el-camino/cards/city/bookmaker/screen2/elem2.png)"></div>
+                </div>
+              </div>
+              <div class="ec-slider__item">
+                <div class="ec-card" v-if="cards.situation === 1">
+                  <div class="ec-card__part" style="background-image: url(img/el-camino/cards/city/bookmaker/screen3/group1/bg.png)"></div>
+                  <div class="ec-card__part ec-comics__slide-part ec-comics__slide-part_anim_f ec-comics__slide-part_delay_6 ec-comics__slide-part_dur_20" style="background-image: url(img/el-camino/cards/city/bookmaker/screen3/group1/elem1.png)"></div>
+                </div>
+                <div class="ec-card" v-else-if="cards.situation === 2">
+                  <div class="ec-card__part" style="background-image: url(img/el-camino/cards/city/bookmaker/screen3/group2/bg.png)"></div>
+                  <div class="ec-card__part ec-comics__slide-part ec-comics__slide-part_anim_f ec-comics__slide-part_delay_6 ec-comics__slide-part_dur_20" style="background-image: url(img/el-camino/cards/city/bookmaker/screen3/group2/elem1.png)"></div>
+                </div>
+                <div class="ec-card" v-else="cards.situation === 3">
+                  <div class="ec-card__part" style="background-image: url(img/el-camino/cards/city/bookmaker/screen3/group3/bg.png)"></div>
+                  <div class="ec-card__part ec-comics__slide-part ec-comics__slide-part_anim_f ec-comics__slide-part_delay_6 ec-comics__slide-part_dur_20" style="background-image: url(img/el-camino/cards/city/bookmaker/screen3/group3/elem1.png)"></div>
                 </div>
               </div>
             </div>
